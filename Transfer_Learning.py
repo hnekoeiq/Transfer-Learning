94336c4b2548aea6cfa3dcfae57e291c0cef1b72
Hadi Nekoei <43013139+hnekoeiq@users.noreply.github.com>
2019-06-12 23:09:50 -0400

    Add files via upload

---
+++ Transfer_Learning.py
@@ -0,0 +1,378 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential, Model 
+from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
+from keras import backend as k 
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
+from keras.datasets import cifar100
+import matplotlib.pyplot as plt
+import scipy.ndimage
+import numpy as np
+from keras.utils import np_utils
+import pickle
+
+from sklearn.decomposition import PCA
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
+
+# In[2]:
+
+
+nb_classes = 100
+
+with open('./train_data', 'rb') as f:
+    train_data = pickle.load(f)
+    train_labels= pickle.load(f)
+with open('./test_data', 'rb') as f:
+    test_data = pickle.load(f)
+x_train = np.transpose(np.reshape(train_data,(-1,3,32,32)), (0,2,3,1))
+x_test = np.transpose(np.reshape(test_data,(-1,3,32,32)), (0,2,3,1))
+y_train = np.reshape(train_labels[:1000],(-1,))
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train = x_train/255.
+x_test = x_test/255.
+
+y_train =  np_utils.to_categorical(y_train, nb_classes)
+
+
+# In[ ]:
+
+
+
+img_width, img_height = 256, 256
+model = applications.inception_v3.InceptionV3(weights = "imagenet", include_top=False, input_shape = (img_width, img_height, 3))
+
+
+# In[ ]:
+
+
+model.summary()
+
+
+# In[ ]:
+
+
+
+# yval =  np_utils.to_categorical(yval, nb_classes)
+steps = 10
+
+xtrain_features = np.zeros((1,2048))
+xtest_features = np.zeros((1,2048))
+
+for i in range(int(x_train.shape[0]/steps)):
+    if i%50 == 0:
+        print(i)
+    xtrain = x_train[steps*(i):steps*(i+1),:,:,:]
+    xtrain = scipy.ndimage.zoom(xtrain, (1,8,8,1), order=0)
+
+    # extract the features
+    features = model.predict(xtrain)
+    features = np.mean(np.reshape(features, (-1,6*6,2048)), axis=1)
+    xtrain_features = np.concatenate((xtrain_features, features), axis=0)
+
+for i in range(int(x_test.shape[0]/steps)):
+    xtest = x_test[steps*(i):steps*(i+1),:,:,:]
+    xtest = scipy.ndimage.zoom(xtest, (1,8,8,1), order=0)
+
+    # extract the features
+    features = model.predict(xtest)
+    features = np.mean(np.reshape(features, (-1,6*6,2048)), axis=1)
+    xtest_features = np.concatenate((xtest_features, features), axis=0)
+
+# ytrain = y_train[steps*(i):steps*(i+1),:]
+# xval = x_train[400:500,:,:,:]
+# yval = y_train[400:500,:]
+#xval = scipy.ndimage.zoom(xval, (1,8,8,1), order=0)
+
+
+# In[ ]:
+
+
+xtrain_features = xtrain_features[1:,:] 
+xtest_features = xtest_features[1:,:] 
+print(xtrain_features.shape)
+print(xtest_features.shape)
+
+np.save('xtrain_features_mean_inceptionv3.npy', xtrain_features)
+np.save('xtest_features_mean_inceptionv3.npy', xtest_features)
+
+
+# In[3]:
+
+
+xtrain_features = np.load('xtrain_features_mean_inceptionv3.npy')
+xtest_features = np.load('xtest_features_mean_inceptionv3.npy')
+
+train_feat = xtrain_features
+#val_feat = xtrain_features[45000:,:]
+test_feat = xtest_features
+
+
+# In[4]:
+
+
+_ , (test , y_test) = cifar100.load_data()
+
+
+# In[ ]:
+
+
+
+# n_components_list = [550, 750, 1024]
+# C_list = [1, 10, 100]
+# for n_components in n_components_list:
+#     for C in C_list:
+#         print(n_components)
+#         print(C)
+#         #Dimensionality Reduction on Features
+#         pca = PCA(n_components=n_components)
+#         components_pca = pca.fit(train_feat)
+#         X_train= pca.transform(train_feat)
+#         X_test= pca.transform(test_feat)
+
+#         #Classifier SVM Linear Kernel 
+#         clf = SVC(C=C, gamma='auto', kernel='rbf')
+#         clf = clf.fit(X_train,train_labels)
+#         predictions_tr = (clf.predict(X_test))
+
+#         #20% Test Data Accuracy
+#         test_acc = accuracy_score(y_test,predictions_tr)
+#         print("Test Accuracy: %0.4f" % test_acc)
+
+
+# In[ ]:
+
+
+# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+# n_components_list = [1024]
+# C_list = [1]
+# for n_components in n_components_list:
+#     for C in C_list:
+#         print(n_components)
+#         #Dimensionality Reduction on Features
+#         pca = PCA(n_components=n_components)
+#         components_pca = pca.fit(train_feat)
+#         X_train= pca.transform(train_feat)
+#         X_test= pca.transform(test_feat)
+        
+#         #Classifier SVM Linear Kernel
+#         clf = LinearDiscriminantAnalysis()
+#         clf.fit(X_train, train_labels)
+#         predictions_tr = (clf.predict(X_test))
+
+#         #20% Test Data Accuracy
+#         test_acc = accuracy_score(y_test,predictions_tr)
+#         print("Test Accuracy: %0.4f" % test_acc)
+
+
+# In[ ]:
+
+
+# from sklearn.manifold import TSNE
+
+# n_components_list = [3]
+# C_list = [1]
+# for n_components in n_components_list:
+#     for C in C_list:
+#         print(n_components)
+#         print(C)
+#         X_train_Embedded_tsne = TSNE(n_components=n_components).fit_transform(train_feat)
+#         X_test_Embedded_tsne = TSNE(n_components=n_components).fit_transform(test_feat)
+        
+#         #Classifier SVM Linear Kernel 
+#         clf = SVC(C=C, gamma='auto', kernel='rbf')
+#         clf = clf.fit(X_train_Embedded,train_labels)
+#         predictions_tr = (clf.predict(X_test_Embedded))
+
+#         #20% Test Data Accuracy
+#         test_acc = accuracy_score(y_test,predictions_tr)
+#         print("Test Accuracy: %0.4f" % test_acc)
+
+
+# In[ ]:
+
+
+# from sklearn.manifold import LocallyLinearEmbedding
+
+# n_components_list = [2, 10, 50, 100]
+# C_list = [1]
+# for n_components in n_components_list:
+#     for C in C_list:
+#         print(n_components)
+#         print(C)
+#         X_train_Embedded_LLE = LocallyLinearEmbedding(n_components=n_components).fit_transform(train_feat)
+#         X_test_Embedded_LLE = LocallyLinearEmbedding(n_components=n_components).fit_transform(test_feat)
+        
+#         print(X_train_Embedded_LLE.shape)
+#         #Classifier SVM Linear Kernel 
+#         clf = SVC(C=C, gamma='auto', kernel='rbf')
+#         clf = clf.fit(X_train_Embedded_LLE,train_labels)
+#         predictions_tr = (clf.predict(X_test_Embedded_LLE))
+
+#         #20% Test Data Accuracy
+#         test_acc = accuracy_score(y_test,predictions_tr)
+#         print("Test Accuracy: %0.4f" % test_acc)
+
+
+# In[15]:
+
+
+def lr_schedule(epoch):
+    
+    lr = 0.1
+    if epoch > 10:
+        lr = lr/10
+    return lr
+
+
+# In[ ]:
+
+
+
+# Define model
+from keras import models
+from keras.models import Sequential
+from keras import layers
+from keras import optimizers
+
+# Freeze the layers which you don't want to train. Here I am freezing the first 5 layers.
+# for layer in model.layers:
+#     layer.trainable = False
+
+# model_checkpoint = ModelCheckpoint("Densenet_model_Checkpoint.h5", monitor="val_acc", save_best_only=True,
+#                                    save_weights_only=True)
+# lr_scheduler = LearningRateScheduler(lr_schedule) 
+# callbacks = [lr_scheduler, model_checkpoint]
+
+
+epochs = 900
+batch_size = 128
+
+model = Sequential()
+model.add(layers.Dense(2048, activation="relu"))
+model.add(layers.Dropout(0.5))
+# model.add(layers.Dense(1024, activation="relu"))
+# model.add(layers.Dropout(0.5))
+model.add(layers.Dense(100, activation="softmax"))
+
+
+# Compile model
+model.compile(optimizer=optimizers.Adam(),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+
+y_train =  np_utils.to_categorical(train_labels, nb_classes)
+y_test =  np_utils.to_categorical(y_test, nb_classes)
+
+
+# train_labels = np.asarray(train_labels)
+# y = np.asarray(y_test)
+# Train model
+history = model.fit(train_feat, y_train,
+                    epochs=epochs,
+                    batch_size=batch_size,
+                    validation_data=(test_feat, y_test))
+model.summary()
+
+
+# In[19]:
+
+
+predictions_tr = model.predict(test_feat)
+predictions_tr = np.argmax(predictions_tr, axis=1)
+
+y_test = np.argmax(y_test, axis=1)
+
+
+# 20% Test Data Accuracy
+test_acc = accuracy_score(y_test,predictions_tr)
+print("Test Accuracy: %0.4f" % test_acc)
+
+
+# In[12]:
+
+
+# # train_data_dir = "data/train"
+# # validation_data_dir = "data/val"
+# nb_train_samples = xtrain.shape[0]
+# nb_validation_samples = xval.shape[0]
+# batch_size = 16
+# epochs = 50
+
+
+# # Freeze the layers which you don't want to train. Here I am freezing the first 5 layers.
+# for layer in model.layers:
+#     layer.trainable = False
+        
+# #Adding custom Layers 
+# x = model.output
+# x = Flatten()(x)
+# x = Dense(1024, activation="relu")(x)
+# x = Dropout(0.5)(x)
+# x = Dense(1024, activation="relu")(x)
+# predictions = Dense(100, activation="softmax")(x)
+
+
+# # creating the final model 
+# model_final = Model(input = model.input, output = predictions)
+
+# # compile the model 
+# model_final.compile(loss = "categorical_crossentropy", optimizer = optimizers.SGD(lr=0.0001, momentum=0.9), metrics=["accuracy"])
+
+
+# # Initiate the train and test generators with data Augumentation 
+# train_datagen = ImageDataGenerator(
+# rescale = 1,
+# horizontal_flip = True,
+# fill_mode = "nearest",
+# zoom_range = 0.3,
+# width_shift_range = 0.3,
+# height_shift_range=0.3,
+# rotation_range=30)
+
+# test_datagen = ImageDataGenerator(
+# rescale = 1,
+# horizontal_flip = True,
+# fill_mode = "nearest",
+# zoom_range = 0.3,
+# width_shift_range = 0.3,
+# height_shift_range=0.3,
+# rotation_range=30)
+
+# train_datagen.fit(x_train)
+
+# train_generator = train_datagen.flow(
+# xtrain, ytrain,
+# batch_size = batch_size)
+
+# validation_generator = test_datagen.flow(
+# xval, yval)
+
+# # Save the model according to the conditions  
+# checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+# early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+
+
+# # Train the model 
+# model_final.fit_generator(
+# train_generator,
+# samples_per_epoch = nb_train_samples,
+# epochs = epochs,
+# validation_data = validation_generator,
+# nb_val_samples = nb_validation_samples,
+# callbacks = [checkpoint, early])
+
